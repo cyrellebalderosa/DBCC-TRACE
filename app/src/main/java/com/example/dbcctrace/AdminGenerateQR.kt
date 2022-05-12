1e5e83741fb2175350771f7bdf5072c2f0de4883
jpmajadas <82078619+jpmajadas@users.noreply.github.com>
2022-05-12 07:28:09 -0700

    Update AdminGenerateQR.kt

--- app/src/main/java/com/example/dbcctrace/AdminGenerateQR.kt
+++ app/src/main/java/com/example/dbcctrace/AdminGenerateQR.kt
@@ -1,5 +1,4 @@
 package com.example.dbcctrace
-
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -12,8 +11,6 @@ import com.example.dbcctrace.databinding.ActivityAdminGenerateQrBinding
 class AdminGenerateQR : AppCompatActivity() {
 
     private lateinit var binding: ActivityAdminGenerateQrBinding
-
-
     private lateinit var tvName: EditText
     private lateinit var tvAddress: EditText
     private lateinit var etTemp: EditText
@@ -26,11 +23,8 @@ class AdminGenerateQR : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAdminGenerateQrBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
+        
         //init
-
         tvName = findViewById(R.id.adminNametv)
         tvAddress = findViewById(R.id.adminaddressTV)
         etTemp = findViewById(R.id.adminetTemp)
@@ -38,16 +32,12 @@ class AdminGenerateQR : AppCompatActivity() {
         ivQRCode = findViewById(R.id.adminivQRCode)
         btngenerate = findViewById(R.id.adminbtngenerate)
 
-
-
         btngenerate.setOnClickListener {
 
             val temp = etTemp.text.toString().trim()
             val name = tvName.text.toString().trim()
             val address = tvAddress.text.toString().trim()
             val event = eventTV.text.toString().trim()
-
-
             val encodertemp = QRGEncoder("FullName: "+ name+ "\n"+ "temperature:"  +temp  + "\n"+ "Address: "+address + "\n" + "Event: "+event , QRGContents.Type.TEXT, 800)
             binding.adminivQRCode.setImageBitmap(encodertemp.bitmap)
 
