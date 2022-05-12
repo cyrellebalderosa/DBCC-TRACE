6e43ffc3a1fa4afc8f7a70c0fa47f8bfff806c17
jpmajadas <82078619+jpmajadas@users.noreply.github.com>
2022-05-12 07:47:50 -0700

    Update ShowNotification.kt

--- app/src/main/java/com/example/dbcctrace/ShowNotification.kt
+++ app/src/main/java/com/example/dbcctrace/ShowNotification.kt
@@ -2,7 +2,6 @@ package com.example.dbcctrace
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
 class ShowNotification : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
