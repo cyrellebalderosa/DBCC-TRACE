bde5034fa9bd877180bbe5f7db00f9228b65fba4
jpmajadas <82078619+jpmajadas@users.noreply.github.com>
2022-05-12 07:46:28 -0700

    Update RecordsMainPage.kt

--- app/src/main/java/com/example/dbcctrace/RecordsMainPage.kt
+++ app/src/main/java/com/example/dbcctrace/RecordsMainPage.kt
@@ -10,12 +10,10 @@ class RecordsMainPage : AppCompatActivity() {
     //viewBinding
     private lateinit var binding: ActivityRecordsMainPageBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecordsMainPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
         binding.userbtn.setOnClickListener {
             startActivity(Intent(this, AdminRecordsPage::class.java))
@@ -25,12 +23,7 @@ class RecordsMainPage : AppCompatActivity() {
             startActivity(Intent(this, QRcodeResult::class.java))
         }
 
-
-
     }
-
-
-
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed() //go back to previous activity, when back button of actionbar is clicked
         return super.onSupportNavigateUp()
