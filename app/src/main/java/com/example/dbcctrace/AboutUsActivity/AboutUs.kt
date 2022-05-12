c6bb705ab22e500a8bd40c52b75e6d04c85bf1ce
jpmajadas <82078619+jpmajadas@users.noreply.github.com>
2022-05-12 07:22:12 -0700

    Update AboutUs.kt

--- app/src/main/java/com/example/dbcctrace/AboutUsActivity/AboutUs.kt
+++ app/src/main/java/com/example/dbcctrace/AboutUsActivity/AboutUs.kt
@@ -1,5 +1,4 @@
 package com.example.dbcctrace.AboutUsActivity
-
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -10,18 +9,15 @@ class AboutUs : AppCompatActivity() {
     //viewBinding
     private lateinit var binding: ActivityAboutUsBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAboutUsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
         binding.aboutus1.setOnClickListener {
             startActivity(Intent(this, AboutUs1::class.java))
         }
-
+        
         binding.aboutus2.setOnClickListener {
             startActivity(Intent(this, AboutUs2::class.java))
         }
@@ -37,11 +33,6 @@ class AboutUs : AppCompatActivity() {
         binding.aboutus5.setOnClickListener {
             startActivity(Intent(this, AboutUs5::class.java))
         }
-
-
-
-
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
