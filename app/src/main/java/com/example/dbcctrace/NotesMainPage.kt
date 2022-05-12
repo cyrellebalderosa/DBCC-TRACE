0b0e9c835a2a8a3bf0e8c7c4ebaf4797a6b36d28
jpmajadas <82078619+jpmajadas@users.noreply.github.com>
2022-05-12 07:43:28 -0700

    Update NotesMainPage.kt

--- app/src/main/java/com/example/dbcctrace/NotesMainPage.kt
+++ app/src/main/java/com/example/dbcctrace/NotesMainPage.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 
 class NotesMainPage : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes_main_page)
@@ -15,7 +14,6 @@ class NotesMainPage : AppCompatActivity() {
         replaceFragment(HomeFragment.newInstance(),false)
 
     }
-
 
     fun replaceFragment(fragment: Fragment, istransition:Boolean){
         val fragmentTransition = supportFragmentManager.beginTransaction()
@@ -33,8 +31,6 @@ class NotesMainPage : AppCompatActivity() {
             finish()
         }
     }
-
-
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed() //go back to previous activity, when back button of actionbar is clicked
         return super.onSupportNavigateUp()
